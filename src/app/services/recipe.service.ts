35cbcda11c2622ce13e3fb1f12093074a092ca58
SRsoz <sozyar.rauf@chasacademy.se>
2025-05-19 14:52:19 +0200

    create recipe service

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Recipe } from '../models/recipe.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RecipeService {
 
-  constructor() { }
-}
+export class RecipeService {
+  private apiUrl = 'http://localhost:5000/api/recipes';
+
+  constructor(private http: HttpClient) {}
+
+// Get all recipes
+  getRecipes(): Observable<Recipe[]> {
+   return this.http.get<Recipe[]>(this.apiUrl);
+  }
+
+  // Get specific recipe
+  getRecipeById(id: string): Observable<Recipe> {
+    return this.http.get<Recipe>(`${this.apiUrl}/${id}`);
+  }
+
+  // Create recipe
+  createRecipe(recipe: Recipe): Observable<Recipe> {
+    return this.http.post<Recipe>(this.apiUrl, recipe);
+  }
+
+  // Update recipe
+  updateRecipe(id: string, recipe: Recipe): Observable<Recipe> {
+    return this.http.put<Recipe>(`${this.apiUrl}/${id}`, recipe);
+  }
+
+  // Delete recipe
+  deleteRecipe(id: string): Observable<{ message: string }> {
+    return this.http.delete<{ message: string }>(`${this.apiUrl}/${id}`);
+  }
+  }
